0306f933b121641a9aeba9cc63a166e4d66c995e
Jorge Encinas <jorge.encinas@devant.com>
2019-11-27 13:41:15 -0600

    Initial Commit

---
+++ RGBullsEye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RGBullsEye
+//
+//  Created by Jorge Encinas on 27/11/19.
+//  Copyright © 2019 Jorge Encinas. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
